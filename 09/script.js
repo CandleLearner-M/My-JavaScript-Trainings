c2b7c87d7abe97c0d6f0ba5cf9b614e55efc173c
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 09:34:09 +0100

    Automated commit: Changes on 2024-12-02 09:34:09

--- 09/script.js
+++ 09/script.js
@@ -1,19 +1,21 @@
 const product = {
-  title: 'Book',
+  title: "Book",
   details: {
     price: 19.99,
     stock: undefined,
     publisher: {
-      name: 'Reading House',
-      year: 2023
-    }
-  }
+      name: "Reading House",
+      year: 2023,
+    },
+  },
 };
 
 /* 
 1. Get the title
-2. Get price and stock (use 'Out of stock' as default for stock)
+2. Get price and stock (use 'Outof stock' as default for stock)
 3. Get the publisher's name and rename it to publisherName
 */
 
 // Write your solution below
+
+TextDecoderStream;
