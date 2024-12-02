e843554bbda96423bafa8592d61a3bca3f691d2e
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:53:34 +0100

    Automated commit: Changes in 2024-12-02 06:53:34

--- 09/script.js
+++ 09/script.js
@@ -2,7 +2,7 @@
 const colors = ['red'];
 
 // Use array destructuring to assign the first color to `primaryColor` and the second color to `secondaryColor` with a default value of 'blue'.
-const [primaryColor = 'blue', secondaryColor = 'blue'] = 
+const [primaryColor = 'blue', secondaryColor = 'blue'] = colors;
 // Log the variables to the console.
 
 // Your code here:
