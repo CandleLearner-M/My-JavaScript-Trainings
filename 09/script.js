86bf7b94f42663e1eaff94b38fc47e210d0042c4
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 07:26:10 +0100

    Automated commit: Changes in 2024-12-02 07:26:10

--- 09/script.js
+++ 09/script.js
@@ -7,5 +7,5 @@ const scores = [10, [20, 30]];
 2. Add a default value of 0 for highScore
 */
 
-const [firstScore, [lowScore ]]
+const [firstScore, [lowScore, highScore = 0]]
 // Write your solution below
