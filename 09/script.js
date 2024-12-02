3cb60500e23f3c80042c75be0f1d72d778d2ceeb
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 08:28:18 +0100

    Automated commit: Changes in 2024-12-02 08:28:18

--- 09/script.js
+++ 09/script.js
@@ -17,5 +17,5 @@ const store = {
 const {name, owner} = store;
 
 // 2. Destructuring city and country from the nested location object
-const {{city, country} = store;
+const {city, country} = store.location;
 // Write your solution below
