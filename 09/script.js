ec3210322106ff1a4778f3ba3d1acb9d396f1095
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 07:15:39 +0100

    Automated commit: Changes in 2024-12-02 07:15:39

--- 09/script.js
+++ 09/script.js
@@ -3,4 +3,4 @@ const numbers = [10, 20, 30];
 // Using destructuring, create three variables named 'first', 'second', and 'third' 
 // that contain the values from this array
 
-const 
+const [first, second, third] = numbers;
