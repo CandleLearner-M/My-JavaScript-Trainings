fb724d65cac0a95d427ca4cdbdc41599d161d7d3
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 07:18:34 +0100

    Automated commit: Changes in 2024-12-02 07:18:34

--- 09/script.js
+++ 09/script.js
@@ -9,5 +9,5 @@ Using destructuring:
 */
 
 // 1.Geting the first team member
-const [firstTeamMember, ,[nestedArrayMember1, nestedArrayMember2], ] = team;
+const [firstTeamMember, ,[nestedArrayMember1, nestedArrayMember2], , ] = team;
 console.log(firstTeamMember);
