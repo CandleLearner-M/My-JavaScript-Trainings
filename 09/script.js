d4b64239cc2e9a99a1139988e2659cdd3235fb51
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:51:56 +0100

    Automated commit: Changes in 2024-12-02 06:51:56

--- 09/script.js
+++ 09/script.js
@@ -2,7 +2,8 @@
 const numbers = [1, 2, 3, 4, 5];
 
 // Use array destructuring to assign the first and third numbers to variables `first` and `third`.
-const [first, , third] = 
+const [first, , third] = numbers;
 // Log the variables to the console.
+
 
 // Your code here:
