0b24fe959eaf0ccc90a3208c47069b06ab8aaef8
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 08:14:08 +0100

    Automated commit: Changes in 2024-12-02 08:14:08

--- 09/script.js
+++ 09/script.js
@@ -1,12 +1,12 @@
-const classroom = ['Physics', ['John', 'Mary'], undefined];
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo 23, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto']
+};
 
-/* 
-1. Get the subject from the first position
-2. Get only the second student name from the nested array
-3. Get the teacher name from the third position, using 'TBD' as default value
-*/
+// 1. Destructure the name and location into variables
+// 2. Destructure categories into a variable called tags
+// 3. Get mainMenu but rename it to menu
 
 // Write your solution below
-
-const [firstSubject, [ , secondStudent], teacher = 'TBD'] = classroom; 
-console.log(firstSubject, secondStudent, teacher)
