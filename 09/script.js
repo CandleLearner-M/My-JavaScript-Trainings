caf66049163cf47bd0ddf54c8a2cd061bbac6055
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 08:14:47 +0100

    Automated commit: Changes in 2024-12-02 08:14:47

--- 09/script.js
+++ 09/script.js
@@ -6,7 +6,7 @@ const restaurant = {
 };
 
 // 1. Destructure the name and location into variables
-const {name, location}
+const {name, location} = restaurant;
 // 2. Destructure categories into a variable called tags
 // 3. Get mainMenu but rename it to menu
 
