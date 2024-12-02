e27f64728b2163c8c3757a26d07f925fbca06be6
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:55:36 +0100

    Automated commit: Changes in 2024-12-02 06:55:36

--- 09/script.js
+++ 09/script.js
@@ -1,11 +1,2 @@
-// Given the following variables:
-let a = 1;
-let b = 2;
-
-// Use array destructuring to swap the values of `a` and `b`.
-const arr = [a, b];
-[b, a] = [a, b]; 
-// Log the variables to the console.
-console.log(a, b);
 
 // Your code here:
