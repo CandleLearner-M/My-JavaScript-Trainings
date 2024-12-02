1fa689b9e6d96f5fbc4d9575bcaa8c45139d531e
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:55:52 +0100

    Automated commit: Changes in 2024-12-02 06:55:52

--- 09/script.js
+++ 09/script.js
@@ -1,2 +1,7 @@
+// Given the following nested array:
+const nestedArray = [1, [2, 3], 4];
+
+// Use array destructuring to assign the value `2` to a variable `nestedValue`.
+// Log the variable to the console.
 
 // Your code here:
