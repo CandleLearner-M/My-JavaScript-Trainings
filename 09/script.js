a2f22c151e8246fbdab8d5498a809b4073f7fcdc
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 08:18:46 +0100

    Automated commit: Changes in 2024-12-02 08:18:46

--- 09/script.js
+++ 09/script.js
@@ -1,14 +1,13 @@
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo 23, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto']
+const cafe = {
+  name: 'Coffee Corner',
+  mainDrink: 'Coffee',
+  // Note: dessert and special are not defined in the object
 };
 
-// 1. Destructure the name and location into variables
-const {name, location} = restaurant;
-// 2. Destructure categories into a variable called tags
-const {categories: tags} = restaurant;
-// 3. Get mainMenu but rename it to menu
-const {mainMenu: menu} = restaurant;
+/* 
+1. Destructure name and mainDrink
+2. Try to destructure dessert with a default value of 'Cake'
+3. Get special and rename it to todaySpecial with a default value of 'No Special'
+*/
+
 // Write your solution below
