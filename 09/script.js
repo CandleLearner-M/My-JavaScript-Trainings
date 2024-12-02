9756352983fddef56ee9f79c40239b745139d703
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 07:16:29 +0100

    Automated commit: Changes in 2024-12-02 07:16:29

--- 09/script.js
+++ 09/script.js
@@ -8,9 +8,3 @@ Using destructuring:
 4. Skip Tom and get Lisa
 */
 
-// Write your solution belowconst numbers = [10, 20, 30];
-
-// Using destructuring, create three variables named 'first', 'second', and 'third' 
-// that contain the values from this array
-
-const [first, second, third] = numbers;
