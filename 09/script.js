c7a5a974a89242b33975f50e327e4dc32c39b381
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 08:17:57 +0100

    Automated commit: Changes in 2024-12-02 08:17:57

--- 09/script.js
+++ 09/script.js
@@ -7,9 +7,8 @@ const restaurant = {
 
 // 1. Destructure the name and location into variables
 const {name, location} = restaurant;
-console.log(name, tata);
 // 2. Destructure categories into a variable called tags
 const {categories: tags} = restaurant;
 // 3. Get mainMenu but rename it to menu
-const {mainMenu: menu}
+const {mainMenu: menu} = restaurant;
 // Write your solution below
