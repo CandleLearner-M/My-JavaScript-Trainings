4e6c9f197236d2586565ca37d03fbe9a22b8e303
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:47:55 +0100

    Automated commit: Changes in 2024-12-02 06:47:55

---
+++ 09/script.js
@@ -0,0 +1,7 @@
+// Given the following array:
+const fruits = ['apple', 'banana', 'cherry'];
+
+// Use array destructuring to assign the first and second fruits to variables `firstFruit` and `secondFruit`.
+// Log the variables to the console.
+
+// Your code here:
