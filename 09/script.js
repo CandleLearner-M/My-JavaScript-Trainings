eeda7a986a75cf25c609b9c37374c008e596b6d6
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-02 06:54:41 +0100

    Automated commit: Changes in 2024-12-02 06:54:41

--- 09/script.js
+++ 09/script.js
@@ -3,7 +3,8 @@ let a = 1;
 let b = 2;
 
 // Use array destructuring to swap the values of `a` and `b`.
-const arr = [a, b]
+const arr = [a, b];
+const 
 // Log the variables to the console.
 
 // Your code here:
