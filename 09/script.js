885980318c18c82440ec5deede76697b4fd1f000
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-09 06:49:42 +0100

    Automated commit: Changes on 2024-12-09 06:49:41

--- 09/script.js
+++ 09/script.js
@@ -3,6 +3,6 @@ const noSpaces = function (str) {
 }
 
 const transformer = function (str, func) {
-  console.log(`Original text: ${fu}`);
+  console.log(`Original text: ${str}`);
   console.log(` Transformed String: ${func(str)}, this was transformed by ${func.name}`);
 }
