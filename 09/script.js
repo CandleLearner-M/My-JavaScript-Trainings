9d62d0c470068a4ee6908cd068ef4e754fc1dac2
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-09 06:49:49 +0100

    Automated commit: Changes on 2024-12-09 06:49:49

--- 09/script.js
+++ 09/script.js
@@ -6,3 +6,5 @@ const transformer = function (str, func) {
   console.log(`Original text: ${str}`);
   console.log(` Transformed String: ${func(str)}, this was transformed by ${func.name}`);
 }
+
+transformer('')
