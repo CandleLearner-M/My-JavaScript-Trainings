e08481ef742d000d22747c89d2d4a1684722f030
Mostafa <candlelearner.m.dev@gmail.com>
2024-12-09 06:50:55 +0100

    Automated commit: Changes on 2024-12-09 06:50:54

--- 09/script.js
+++ 09/script.js
@@ -4,7 +4,7 @@ const noSpaces = function (str) {
 
 const transformer = function (str, func) {
   console.log(`Original text: ${str}`);
-  console.log(` Transformed String: ${func(str)}, this was transformed by ${func.name}`);
+  console.log(`Transformed String: ${func(str)}, this was transformed by ${func.name}`);
 }
 
 transformer('Mostafa is The best one!', noSpaces);
